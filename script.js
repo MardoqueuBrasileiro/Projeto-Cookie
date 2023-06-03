12e6c7aa288fd29d1db8ca791b45bc94d614ce40
MardoqueuBrasileiro <121585197+MardoqueuBrasileiro@users.noreply.github.com>
2023-06-03 15:22:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-let score = 0;
+// Configure o Firebase com suas credenciais
+var firebaseConfig = {
+  apiKey: "AIzaSyAwLyVDy4foa2D8LZk0vmia2MAbFyVskLE",
+  authDomain: "projeto-cookie.firebaseapp.com",
+  projectId: "1:26889422861:web:97aa8d128a6738a2e4ad44",
+  storageBucket: "projeto-cookie.appspot.com",
+  messagingSenderId: "BFKwUNST3F4WKnDB57ezb8kZlQm2cXHGV_LnNFa6K5k0tcLy4_oS1ENFopr82wyp5eAzGdJpO42a-cW1QwVPi-w",
+  appId: "26889422861"
+};
 
-const scoreElement = document.getElementById('score');
-const cookieButton = document.getElementById('cookieButton');
+firebase.initializeApp(firebaseConfig);
 
-cookieButton.addEventListener('click', () => {
-  score++;
-  scoreElement.innerText = 'Score: ' + score;
+// Obtenha uma referência para o serviço de autenticação do Firebase
+var auth = firebase.auth();
+
+// Elementos da página
+var loginEmailField = document.getElementById('loginEmail');
+var loginPasswordField = document.getElementById('loginPassword');
+var loginButton = document.getElementById('loginButton');
+
+var registerEmailField = document.getElementById('registerEmail');
+var registerPasswordField = document.getElementById('registerPassword');
+var registerButton = document.getElementById('registerButton');
+
+// Evento de login
+loginButton.addEventListener('click', function() {
+  var email = loginEmailField.value;
+  var password = loginPasswordField.value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      alert('Login bem-sucedido!');
+      // Redirecionar para a página de jogo, por exemplo
+    })
+    .catch(function(error) {
+      alert('Erro no login: ' + error.message);
+    });
 });
+
+// Evento de registro
+registerButton.addEventListener('click', function() {
+  var email = registerEmailField.value;
+  var password = registerPasswordField.value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      alert('Registro bem-sucedido!');
+      // Redirecionar para a página de jogo, por exemplo
+    })
+    .catch(function(error) {
+      alert('Erro no registro: ' + error.message);
+    });
+});
+
